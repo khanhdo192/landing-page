633d5d461c9c7262d07dd65754d79cc4244b2f6e
Khanh Do <khanhdo1219@gmail.com>
2022-06-02 13:56:22 +0700

    fix upload api

--- assets/js/api/userApi.js
+++ assets/js/api/userApi.js
@@ -81,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Validator.isRequired('#pwNew'),
     ],
     onSubmit: async (data) => {
-      console.log(data);
+      // console.log(data);
       const updateData = {
         name: data.name,
         password: data.passwordNew || data.passwordOld,
@@ -90,10 +90,10 @@ document.addEventListener('DOMContentLoaded', () => {
       delete updateData.passwordOld;
       delete updateData.passwordNew;
       delete updateData.passwordCheck;
-      console.log(updateData);
-      // await axios.put(apiUrl + 'users/' + data.id, updateData);
-      // localStorage.setItem('userInfo', JSON.stringify(updateData));
-      // window.location.reload();
+      // console.log(updateData);
+      await axios.put(apiUrl + 'users/' + data.id, updateData);
+      localStorage.setItem('userInfo', JSON.stringify(updateData));
+      window.location.reload();
     },
   });
 });
